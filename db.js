f899f1040b7b77128f46d74a23b47d8393b856db
sailendrachettri <sailendra9083@gmail.com>
2024-03-23 11:15:45 +0530

    production and development db logic added

--- db.js
+++ db.js
@@ -3,7 +3,12 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const connect = mongoose.connect(process.env.REACT_APP_MONGODB_URL)
+let connect;
+if(process.env.REACT_APP_NODE_ENV == "production")
+    connect = mongoose.connect(process.env.REACT_APP_MONGODB_URL_PROD)
+else
+    connect = mongoose.connect(process.env.REACT_APP_MONGODB_URL_DEV)
+    
 
 connect.then(()=>{
     console.log("Database connected successfully!");
